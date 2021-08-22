fa044eee4ffc3597e3459445588fabd329f31a7f
huang-hanson <57970696+huang-hanson@users.noreply.github.com>
2021-08-22 14:02:37 +0800

    8/22

---
+++ java_basic/src/com/hanson/DataStructure/Bitmap.java
@@ -0,0 +1,12 @@
+package com.hanson.DataStructure;
+
+/**
+ * @author 黄忠
+ */
+/*以bit为存储单位的数据结构，对于给定的第i位。1表示true，0表示false*/
+public class Bitmap {
+    private byte[] bytes;
+    //length为位图的长度，实际可操作的下表为[0,length)
+    private int length;
+
+}
